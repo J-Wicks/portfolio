60ed8fae286e692196876992565c85de225b7fc5
Jeremy Wicks <artfullu@gmail.com>
2017-09-22 13:13:52 -0400

    got that bar working

--- src/Projects.js
+++ src/Projects.js
@@ -4,13 +4,16 @@ import Reveal from 'react-reveal'; // this package
 class SocialMedia extends Component {
   render() {
     return (
-      <div id="projects">
-        <h1 className='sectionHead'> Projects </h1>
-          <div className="abbrevCircle">
-          <p>Ch</p>
-          </div>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/Hhie8-Py3NM" frameborder="0" allowfullscreen></iframe>
-      </div>
+        <section id="projects">
+          <li className='projectList'>
+            <h1 className='sectionHead'> Projects </h1>
+            <hr width="1" size="500" />
+            <div className="abbrevCircle">
+              <p>Ch</p>
+              </div>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/Hhie8-Py3NM" frameborder="0" allowfullscreen></iframe>
+          </li>
+        </section>
     );
   }
 }
